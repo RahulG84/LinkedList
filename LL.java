7a7202a6bb8ec4776f38d7785e308faeb7ce84d5
Your Name <rahul4girgal@gmail.com>
2022-07-12 21:22:21 +0530

    Added : LinkedList By Appending 30-70-56

--- LL.java
+++ LL.java
@@ -15,15 +15,31 @@ public class LL {
         head = t;
         size++;
     }
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        newNode.data = data;
+        newNode.next = null;
+        if (size == 0) {
+            head = tail = newNode;
+        }
+        else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
 
     public void show() {
-        Node temp = head;
         if (head == null) {
             System.out.println("List Is Empty");
+            return;
+        }
+            Node temp = head;
             while (temp != null) {
                 System.out.print(temp.data + " ");
                 temp = temp.next;
             }
+        System.out.println("NULL");
         }
     }
-}
+
--- MainMethod.java
+++ MainMethod.java
@@ -2,11 +2,10 @@ package com.bl.linkedlist;
 import java.util.LinkedList;
 public class MainMethod {
     public static void main(String[] args) {
-
         LL ll = new LL();
-        ll.addFirst(70);
-        ll.addFirst(30);
-        ll.addFirst(56);
+        ll.addLast(56);
+        ll.addLast(30);
+        ll.addLast(70);
         ll.show();
     }
 }
